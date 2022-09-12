db563a7431970d3355bf76d0a73b3f77ba7111d0
Wonjong Mo <wmo@c8r5s13.42seoul.kr>
2022-09-12 23:44:18 +0900

    temp dp

--- dp.c
+++ dp.c
@@ -6,11 +6,11 @@
 /*   By: wmo <wmo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 21:14:11 by wmo               #+#    #+#             */
-/*   Updated: 2022/09/12 23:31:58 by wmo              ###   ########.fr       */
+/*   Updated: 2022/09/12 23:43:08 by wmo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int min(int a, int b, int c)
+int	min(int a, int b, int c)
 {
 	if (a > b)
 		a = b;
@@ -41,31 +41,32 @@ int	**init_dp(char **tab, int **d, t_info info)
 	return (d);
 }
 
-char	**ft_dpdp(char **tab, int **d, t_info info, t_point p)
+char	**ft_dpdp(char **tab, int **d, t_info *info, t_point *p)
 {
 	int	i;
 	int	j;
 
 	i = 1;
 	while (i < info->line)
+	{
+		j = 1;
+		while (tab[i][j])
 		{
-			j = 1;
-			while (tab[i][j])
+			if (tab[i][j] != info->obs && tab[i - 1][j - 1] != info->obs && \
+					tab[i - 1][j] != info->obs && tab[i][j - 1] != info->obs)
 			{
-				if (tab[i][j] != 'o' && tab[i-1][j-1] != 'o' && tab[i-1][j] != 'o' && tab[i][j-1] != 'o')
+				d[i][j] = min(d[i - 1][j - 1], d[i - 1][j], d[i][j - 1]) + 1;
+				if (d[i][j] > p->max)
 				{
-					d[i][j] = min(d[i-1][j-1], d[i-1][j], d[i][j-1]) + 1;
-					if (d[i][j] > p->max)
-					{
-						p->max = d[i][j];
-						p.->max_row = i;
-						p->max_col = j;
-					}
+					p->max = d[i][j];
+					p.->max_row = i;
+					p->max_col = j;
 				}
-				j++;
 			}
-			i++;
+			j++;
 		}
+		i++;
+	}
 	return (d);
 }
 
@@ -73,7 +74,7 @@ char	**ft_dp(char **tab, int **d, t_info info)
 {
 	int	i;
 	int	j;
-	t_point p;
+	t_point	p;
 
 	p.max_row = 0;
 	p.max_col = 0;
@@ -83,7 +84,7 @@ char	**ft_dp(char **tab, int **d, t_info info)
 	while (i <= p.max_row)
 	{
 		j = p.max_col - (p.max - 1);
-		while (j <=p.max_row)
+		while (j <= p.max_row)
 		{
 			tab[i][j] = info.ful;
 			j++;
@@ -92,13 +93,3 @@ char	**ft_dp(char **tab, int **d, t_info info)
 	}
 	return (tab);
 }
-
-int min(int a, int b, int c)
-{
-	if (a > b)
-		a = b;
-	if (a > c)
-		a = c;
-	return (a);
-}
-	
